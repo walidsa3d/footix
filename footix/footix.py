5e3df0e8684fb768da961242e48de3b91463f581
walid <walid.sa3d@gmail.com>
2015-09-07 13:16:24 +0100

    first commit

--- footix/footix.py
+++ footix/footix.py
@@ -7,32 +7,36 @@ from bs4 import BeautifulSoup as bs
 from termcolor import colored
 import argparse
 
-url="http://www.getyourfixtures.com/all/fixtures/today/football"
-timezone_url="http://www.getyourfixtures.com/setTimeZone.php?offset=%2B01%3A00"
+base_url="http://www.getyourfixtures.com/all/fixtures/today/football"
+offset='+01:00'
+offset=requests.utils.quote(offset)
+timezone_url="http://www.getyourfixtures.com/setTimeZone.php?offset={}".format(offset)
+
 def get_data():
     matches=[]
     session=requests.Session()
     session.get(timezone_url)
-    response=session.get(url)
+    response=session.get(base_url)
     soup=bs(response.content,"lxml")
     for div in soup.find_all("div",{"class":"match"}):
-      if div.select("div.home") and div.select("div.away"):
-        time=div.select("div.time span")[0].get_text().strip()
-        home=div.select("div.home")[0].get_text().strip()
-        away=div.select("div.away")[0].get_text().strip()
-        stations=div.select("div.stations ul li.country-qa")
-        if stations:
-            stations=[station.text.strip().replace('BeIN Sport Arabia','BeIN') for station in stations]
-        else:
-            stations=[]
-        matches.append({"time":time,"station":stations,"home":home,"away":away})
+        if div.select("div.home") and div.select("div.away"):
+            time=div.select("div.time span")[0].get_text().strip()
+            home=div.select("div.home")[0].get_text().strip()
+            away=div.select("div.away")[0].get_text().strip()
+            stations=div.select("div.stations ul li.country-qa")
+            if stations:
+                stations=[station.text.strip().replace('BeIN Sport Arabia','BeIN') for station in stations]
+            else:
+                stations=[]
+            matches.append({"time":time,"station":stations,"home":home,"away":away})
     return matches
+
 def main():
     data=get_data()
     parser = argparse.ArgumentParser(usage="-h for full usage")
     parser.add_argument('-v', help='search query',action="store_true")
     args = parser.parse_args()
-    if args.view:
+    if args.v:
         data=[x for x in data if x['station']]
     args = parser.parse_args()
     for match in data:
@@ -41,5 +45,4 @@ def main():
         first_team = colored(match['home'], "green")
         second_team = colored(match['away'], "cyan")
         vs = colored('VS', "white", attrs=['bold', 'blink'])
-        print u"{:10} {} {:40} {:30} {:40}".format(match_time, channels, first_team, vs, second_team)
-main()
+        print u"{:10} {:40} {:40} {:30} {:40}".format(match_time, channels, first_team, vs, second_team)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ data_files = [
 
 setup(
     name='footix',
-    version='0.9',
+    version='1.0',
     description="soccer schedule",
     long_description=open('README.md').read(),
     author='Walid Saad',
@@ -18,17 +18,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     data_files=data_files,
-    entry_points={"console_scripts": ["footix=footix.footix:main"]},
-    classifiers=[
-        'Development Status :: 4  - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Topic :: Multimedia :: Video',
-        'Topic :: Utilities'
-    ]
+    entry_points={"console_scripts": ["footix=footix.footix:main"]}
 )
